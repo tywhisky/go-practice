ff185861526820d0c81dc074220fac9777ed70d7
tywhisky <fty221@gmail.com>
2023-03-19 17:13:32 +0800

    Composite Types

---
+++ 4.Composite Types/rev.go
@@ -0,0 +1,19 @@
+package main
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return false
+}
